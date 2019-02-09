900b76c5351e11ab8e41446ce53c37c31d161f21
Thomas Quandt <thquad@gmail.com>
2019-02-09 14:15:17 +0100

    Add a sphere with controls

---
+++ Taliturtle/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody p_rigidbody;
+    public float m_speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        p_rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Update 
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        moveHorizontal *= m_speed;
+        moveVertical *= m_speed;
+
+        p_rigidbody.AddForce(new Vector3(moveHorizontal,0,moveVertical));
+    }
+}
